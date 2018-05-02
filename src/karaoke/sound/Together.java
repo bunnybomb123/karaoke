ee955f129d1955861ba668dda9778d204d3808df
EricaWeng <ericaxw99@gmail.com>
2018-05-02 18:55:51 -0400

    added together

---
+++ src/karaoke/sound/Together.java
@@ -0,0 +1,29 @@
+package karaoke.sound;
+
+
+public class Together implements Music {
+
+    private Music m1;
+    private Music m2;
+
+    public Together(Music m1, Music m2) {
+        this.m1 = m1;
+        this.m2 = m2;
+    }
+
+    public <R> R accept(Visitor<R> visitor) {
+        // TODO Auto-generated method stub
+        return visitor.on(this);
+    }
+
+    public double duration() {
+        // TODO Auto-generated method stub
+        return Math.max(m1.duration(), m2.duration());
+    }
+
+    public void play(SequencePlayer player, double atBeat) {
+        // TODO Auto-generated method stub
+
+    }
+
+}

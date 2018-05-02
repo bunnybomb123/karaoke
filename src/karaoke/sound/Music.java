4920bdef3a9c1de1ef71ed81ddc6aa1223ea09af
EricaWeng <ericaxw99@gmail.com>
2018-05-02 12:17:23 -0400

    files that are useful from lecture 27 music II

---
+++ src/karaoke/sound/Music.java
@@ -0,0 +1,39 @@
+package karaoke.sound;
+
+/**
+ * Music represents a piece of music played by multiple instruments.
+ */
+public interface Music {
+
+    /**
+     * A function from Music to R, defined by cases for each variant.
+     * @param <R> the type of the result of the function
+     */
+    public interface Visitor<R> {
+        public R on(Concat concat);
+        public R on(Note note);
+        public R on(Rest rest);
+        public R on(Together together);
+    }
+
+    /**
+     * Apply a function (represented by a visitor) to this music.
+     * @param <R> the type of the result
+     * @param visitor function to apply
+     * @return the result of applying visitor on this
+     */
+    public <R> R accept(Visitor<R> visitor);
+
+    /**
+     * @return total duration of this piece in beats
+     */
+    double duration();
+
+    /**
+     * Play this piece.
+     * @param player player to play on
+     * @param atBeat when to play
+     */
+    void play(SequencePlayer player, double atBeat);
+
+}

9b2282e0d38356cffb6ce7e6e0df1bb77df7d3a2
Jordan Wick <wickj@mit.edu>
2018-05-02 20:10:13 -0400

    Added measure.java

---
+++ src/karaoke/sound/Measure.java
@@ -0,0 +1,28 @@
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Measure implements Music {
+    
+    private final Music music;
+    
+    /**
+     * Constructs a Measure object to wrap around the given Music object
+     * @param music
+     */
+    public Measure(Music music) {
+        this.music = music;
+    }
+
+    @Override
+    public double duration() {
+        return music.duration();
+    }
+
+    @Override
+    public void play(SequencePlayer player, double atBeat, Consumer<String> lyricConsumer) {
+        music.play(player, atBeat, lyricConsumer);
+    }
+    
+    
+}

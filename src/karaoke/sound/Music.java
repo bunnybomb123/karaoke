ee519dc4683537df167b4d9e4e13ae5792d5e89b
Christopher Chang <christopherchang9@gmail.com>
2018-05-07 12:34:44 -0400

    completed Music ADT except for tiny parts of specs

--- src/karaoke/sound/Music.java
+++ src/karaoke/sound/Music.java
@@ -19,12 +19,31 @@ public interface Music {
      * A function from Music to R, defined by cases for each variant.
      * @param <R> the type of the result of the function
      */
-    /*public interface Visitor<R> {
+    public interface Visitor<R> {
+        /**
+         * @param concat music as Concat
+         * @return result of calling visitor on Concat
+         */
         public R on(Concat concat);
+        
+        /**
+         * @param note music as Note
+         * @return result of calling visitor on Note
+         */
         public R on(Note note);
+        
+        /**
+         * @param rest music as Rest
+         * @return result of calling visitor on Rest
+         */
         public R on(Rest rest);
+        
+        /**
+         * @param together music as Together
+         * @return result of calling visitor on Together
+         */
         public R on(Together together);
-    }*/
+    }
 
     /**
      * Apply a function (represented by a visitor) to this music.
@@ -32,7 +51,7 @@ public interface Music {
      * @param visitor function to apply
      * @return the result of applying visitor on this
      */
-    /*public <R> R accept(Visitor<R> visitor);*/
+    public <R> R accept(Visitor<R> visitor);
 
     /**
      * @return total duration of this piece in beats
--- src/karaoke/sound/Together.java
+++ src/karaoke/sound/Together.java
@@ -1,30 +1,86 @@
 package karaoke.sound;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 
+/**
+ * Concat represents two pieces of music played simultaneously.
+ */
 public class Together implements Music {
 
-    private Music m1;
-    private Music m2;
-
-    public Together(Music m1, Music m2) {
-        this.m1 = m1;
-        this.m2 = m2;
+    private final Music first;
+    private final Music second;
+    
+    private void checkRep() {
+        assert first != null;
+        assert second != null;
     }
 
-    /*public <R> R accept(Visitor<R> visitor) {
-        // TODO Auto-generated method stub
+    /**
+     * Make a Music sequence that plays first at the same time as second.
+     * @param first first music to play, defines this Music's duration
+     * @param second second music to play
+     */
+    public Together(Music first, Music second) {
+        this.first = first;
+        this.second = second;
+        checkRep();
+    }
+
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
         return visitor.on(this);
-    }*/
-
-    public double duration() {
-        // TODO Auto-generated method stub
-        return m1.duration();
     }
 
-    public void play(SequencePlayer player, double atBeat, Consumer<Lyric> lyricConsumer) {
-        // TODO Auto-generated method stub
+    /**
+     * @return first piece in this concatenation
+     */
+    public Music first() {
+        return first;
+    }
 
+    /**
+     * @return second piece in this concatenation
+     */
+    public Music second() {
+        return second;
+    }
+    
+    /**
+     * @return duration of this chord, defined to be the duration of the first note
+     */
+    @Override
+    public double duration() {
+        return first.duration();
+    }
+
+    /**
+     * Play this chord.
+     */
+    @Override
+    public void play(SequencePlayer player, double atBeat, Consumer<Lyric> lyricConsumer) {
+        first.play(player, atBeat, lyricConsumer);
+        second.play(player, atBeat, lyricConsumer);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        
+        final Together other = (Together) obj;
+        return first.equals(other.first) && second.equals(other.second);
+    }
+
+    @Override
+    public String toString() {
+        return first.toString() + second;
     }
 
 }

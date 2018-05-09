250051d057409f6e71e387f7d4e459aa4ccf6490
Jordan Wick <wickj@mit.edu>
2018-05-09 12:31:06 -0400

    Added tests for WebServer

--- test/karaoke/web/WebServerTest.java
+++ test/karaoke/web/WebServerTest.java
@@ -1,8 +1,9 @@
 package karaoke.web;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals; 
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
@@ -24,8 +25,11 @@ import karaoke.sound.Pitch;
 public class WebServerTest {
     
     /*
-     * Testing Strategy for WebServer:
+     * Testing Strategy for WebServer.java:
+     * 
+     * Strategy for WebServer constructor:
      * Partitions: 
+     * port number: valid port number, invalid port number, used port number
      * Input files: valid file, invalid file
      * Number of input files: 0, 1, 2+ 
      * 
@@ -36,11 +40,34 @@ public class WebServerTest {
      * 
      */
     
+    private int PORT_NUMBER = 8080;
+    
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
     
+    @Test
+    public void testStartServerNoError() throws IOException {
+        // Tests to make sure 
+        karaoke.WebServer server = new karaoke.WebServer(PORT_NUMBER);
+        
+    }
+    
+    @Test
+    public void testStartTwoServersSamePort() throws IOException {
+        // Tests to make sure an error is thrown when two servers listen on the same port
+        
+        karaoke.WebServer server = new karaoke.WebServer(PORT_NUMBER);
+        try {
+            // Should fail here
+            karaoke.WebServer secondServer = new karaoke.WebServer(PORT_NUMBER);
+            assert false;
+        } catch (IOException e) {
+            
+        }
+    }
+        
     
     @Test
     public void testPlayFile() {
@@ -50,6 +77,10 @@ public class WebServerTest {
         // Wait a certain amount of time
         // Make sure it ends
         // Make sure it printed all the correct lyrics
+    }
+    
+    @Test public void testPlayInvalidFile() {
+        
     }
     
     

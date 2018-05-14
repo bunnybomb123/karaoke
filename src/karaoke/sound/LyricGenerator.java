d9e4d1bf3502dc2e0449a4546db731354b58c4e9
Christopher Chang <christopherchang9@gmail.com>
2018-05-14 16:55:34 -0400

    finished LyricGenerator

--- src/karaoke/sound/LyricGenerator.java
+++ src/karaoke/sound/LyricGenerator.java
@@ -5,33 +5,35 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Lyric represents either a lyrical line (including the syllable being sung, if any) or the absence of a lyric during an instrumental.
+ * LyricGenerator generates Lyrics from a provided lyrical line.
  */
 public class LyricGenerator {
     
     private final List<String> lyricalElements;
     private final String line;
-    private int index = 0;
+    private int index;
     private int beginIndex = 0;
     private int hold = 0;
     
     /* Abstraction function:
-     *  AF(prefix, syllable, suffix, isInstrumental) =
-     *      a lyric if isInstrumental is false,
-     *          represented by the tuple (prefix, syllable, suffix)
-     *          denoting the lyrical line prefix + syllable.get() + suffix where
-     *          syllable is the syllable being sung, if any; or
-     *      the absence of a lyric if isInstrumental is true.
+     *  AF(lyricalElements, line, index, beginIndex, hold) =
+     *      a lyric generator that generates lyrics from the lyrical line
+     *          specified by lyricalElements and with formatted representation line,
+     *      where the syllable being sung by the next lyric (or the barline if at the end of a measure)
+     *          is represented by lyricalElements.get(index)
+     *          and found in the formatted lyrical line as line.substring(beginIndex, beginIndex + syllable.length()),
+     *          unless hold > 0 in which case the previous syllable should be held for hold more notes
      * 
      * Rep invariant:
      *  fields are not null
+     *  if index < lyricalElements.size():
+     *      symbol = lyricalElements.get(index) is either a syllable or a barline, and
+     *      syllable = format(symbol) is equal to line.substring(beginIndex, beginIndex + syllable.length())
+     *  hold >= 0
      * 
      * Safety from rep exposure:
-     *  all fields are final and immutable
-     *  
-     * Thread safety argument:
-     *  This object and its field are all immutable, and there is no 
-     *  beneficent mutation
+     *  defensive copying in constructor for lyricalElements
+     *  no other fields are passed in or returned by any methods
      */
     
     /**
@@ -44,17 +46,25 @@ public class LyricGenerator {
         StringBuffer formattedLine = new StringBuffer();
         lyricalElements.stream().map(LyricGenerator::format).forEach(formattedLine::append);
         this.line = formattedLine.toString();
+        this.index = 0;
+        removeSuffix();
+        checkRep();
     }
     
+    /*
+     * Check rep invariant.
+     */
     private void checkRep() {
         assert lyricalElements != null;
         assert line != null;
-        String prev = index > 0 ? lyricalElements.get(index - 1) : null;
-        String symbol = lyricalElements.get(index);
-        assert !isSuffix(symbol, prev);
-        String syllable = format(symbol);
-        int endIndex = beginIndex + syllable.length();
-        assert syllable.equals(line.substring(beginIndex, endIndex));
+        if (index < lyricalElements.size()) {
+            String prev = index > 0 ? lyricalElements.get(index - 1) : null;
+            String symbol = lyricalElements.get(index);
+            assert !isSuffix(symbol, prev);
+            String syllable = format(symbol);
+            int endIndex = beginIndex + syllable.length();
+            assert syllable.equals(line.substring(beginIndex, endIndex));
+        }
         assert hold >= 0;
     }
     
@@ -66,10 +76,11 @@ public class LyricGenerator {
     public Optional<Lyric> next() {
         if (hold > 0) {
             hold--;
+            checkRep();
             return Optional.empty();
         }
         
-        if (lyricalElements.get(index).equals("|"))
+        if (index >= lyricalElements.size() || lyricalElements.get(index).equals("|"))
             return Optional.of(Lyric.INSTRUMENTAL);
         
         return nextSyllable();
@@ -81,35 +92,50 @@ public class LyricGenerator {
      * instead of instrumental Lyrics.
      */
     public void loadNextMeasure() {
-        if (lyricalElements.get(index).equals("|")) {
+        if (index < lyricalElements.size() && lyricalElements.get(index).equals("|")) {
             hold = 0;
             nextSyllable();
+            checkRep();
         }
     }
     
+    /*
+     * Returns a new Lyric representing the extended syllable starting at lyricalElements.get(index) being sung.
+     * An extended syllable is a syllable extended to include all "_" characters following it.
+     * Updates index to point to next syllable or barline.
+     */
     private Optional<Lyric> nextSyllable() {
-        String syllable = format(lyricalElements.get(index));
+        String syllable = format(lyricalElements.get(index++));
         String suffix = removeSuffix();
         int endIndex = beginIndex + syllable.length() + suffix.lastIndexOf('_') + 1;
         Lyric lyric = new Lyric(line, beginIndex, endIndex);
         beginIndex += syllable.length() + suffix.length();
+        checkRep();
         return Optional.of(lyric);
     }
     
+    /*
+     * Returns the suffix starting at lyricalElements.get(index).
+     * A suffix is a string of characters that could trail behind a syllable or barline before the next syllable or barline.
+     * Updates index to point to next syllable or barline.
+     */
     private String removeSuffix() {
-        String suffix = "";
-        String prev = lyricalElements.get(index);
-        String symbol = lyricalElements.get(++index);
+        StringBuffer suffix = new StringBuffer();
+        String prev = index > 0 ? lyricalElements.get(index - 1) : null;
+        String symbol = lyricalElements.get(index);
         while (isSuffix(symbol, prev)) {
-            suffix += symbol;
+            suffix.append(symbol);
             if (symbol.equals("_"))
                 hold++;
             prev = symbol;
             symbol = lyricalElements.get(++index);
         }
-        return suffix;
+        return suffix.toString();
     }
     
+    /*
+     * Given a symbol and the symbol preceding it, return whether the symbol is part of a suffix.
+     */
     private static boolean isSuffix(String symbol, String prev) {
         return symbol.equals("-") && (prev == null ||
                                       !prev.trim().isEmpty() && !prev.equals("-"))
@@ -117,6 +143,10 @@ public class LyricGenerator {
                 || symbol.equals("_");
     }
     
+    /**
+     * @param symbol lyrical element to parse
+     * @return formatted representation of symbol
+     */
     private static String format(String symbol) {
         return symbol.replace("~", " ").replace("\\-", "-");
     }

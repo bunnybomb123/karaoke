5fefc0e63b4ebc1a47657f79a268ce43d3a1de15
Erica Weng <ericaw@mit.edu>
2018-05-14 22:55:14 -0700

    LyricGeneratorTest

--- test/karaoke/lyrics/LyricGeneratorTest.java
+++ test/karaoke/lyrics/LyricGeneratorTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertTrue;
  * tests Lyrics 
  * @author ericaweng
  */
-public class LyricsGeneratorTest {
+public class LyricGeneratorTest {
 	
     /* Testing strategy
      *  input:
@@ -36,6 +36,7 @@ public class LyricsGeneratorTest {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
     
+    /*  */
     private void helperTest(List<Lyric> expected, LyricGenerator lg) {
     	for (Lyric l : expected) {
     		System.out.println(l.getSyllable());
@@ -43,38 +44,36 @@ public class LyricsGeneratorTest {
     	}
     }
     
-    private void helperLyric() {
-    }
-    
     @Test
     public void testWaxiesDargle() {
     	LyricGenerator lg = new LyricGenerator("1");
-    	lg.loadNewLine(Arrays.asList("I'll go down to Mon-to to-w-n To see un-cle Mc-Ar-dle A-nd".split(" ")));
+    	lg.loadNewLine(Arrays.asList("I'll", " ","go"," ","down"," ","to"," ","Mon",
+    			"-","to"," ", "to","-","w","-","n"," ","To"," ","see"," ","un","-","cle",
+    			" ","Mc","-","Ar","-","dle"," ","A","-","nd"));
     	
     	String voice = "1";
-    	String line = "I'll go down to Mon-to to-w-n To see un-cle Mc-Ar-dle A-nd".replaceAll(" ", "");
+    	String line = "I'll go down to Mon-to to-w-n To see un-cle Mc-Ar-dle A-nd";
     	List<Lyric> expected = Arrays.asList(
 			new Lyric(voice, line, 0, 4), // I'll
-			new Lyric(voice, line, 4, 6), // go
-			new Lyric(voice, line, 6, 10), // down
-			new Lyric(voice, line, 10, 12), // to
-			new Lyric(voice, line, 12, 15), // Mon
-			new Lyric(voice, line, 16, 18), // to
-			new Lyric(voice, line, 18, 20), // to
-			new Lyric(voice, line, 21, 22), // w
-			new Lyric(voice, line, 23, 24), // n
-			new Lyric(voice, line, 24, 26), // To
-			new Lyric(voice, line, 26, 29), // see
-			new Lyric(voice, line, 29, 31), // un
-			new Lyric(voice, line, 32, 35), // cle
-			new Lyric(voice, line, 35, 37), // Mc
-			new Lyric(voice, line, 38, 40), // Ar
-			new Lyric(voice, line, 41, 44), // dle
-			new Lyric(voice, line, 44, 45), // A
-			new Lyric(voice, line, 46, 48) // nd
+			new Lyric(voice, line, 5, 7), // go
+			new Lyric(voice, line, 8, 12), // down
+			new Lyric(voice, line, 13, 15), // to
+			new Lyric(voice, line, 16, 19), // Mon
+			new Lyric(voice, line, 20, 22), // to
+			new Lyric(voice, line, 23, 25), // to
+			new Lyric(voice, line, 26, 27), // w
+			new Lyric(voice, line, 28, 29), // n
+			new Lyric(voice, line, 30, 32), // To
+			new Lyric(voice, line, 33, 36), // see
+			new Lyric(voice, line, 37, 39), // un
+			new Lyric(voice, line, 40, 43), // cle
+			new Lyric(voice, line, 44, 46), // Mc
+			new Lyric(voice, line, 47, 49), // Ar
+			new Lyric(voice, line, 50, 53), // dle
+			new Lyric(voice, line, 54, 55), // A
+			new Lyric(voice, line, 56, 58) // nd
 		);
     	helperTest(expected, lg);
-    	
     	
     }
 }

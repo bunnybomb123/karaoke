2d3a4bff82b8e89044e1c0ad5084dc42ffd7923c
Christopher Chang <christopherchang9@gmail.com>
2018-05-15 00:20:31 -0400

    Key progress

--- src/karaoke/songs/Key.java
+++ src/karaoke/songs/Key.java
@@ -2,11 +2,12 @@ package karaoke.songs;
 
 
 public enum Key {
-    C(0), Am(0),
-    G(1), Em(1),
-    D(2), Bm(2),
-    A(3), F$m(3),
-    E(4), C$m(4),
-    B(5), G$m(5),
-    F$(6), D$m(6)
+    C(0), Bb(0),  Am(0),
+    G(1),         Em(1),
+    D(2),         Bm(2), Cbm(2),
+    A(3),         F$m(3), Gbm(3),
+    E(4), Fb(4),  C$m(4),
+    B(5), Cb(5),  G$m(5), Abm(5),
+    F$(6), Gb(6), D$m(6), Ebm(6),
+    
 }

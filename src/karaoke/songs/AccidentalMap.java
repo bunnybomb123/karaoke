05b4e6ca2345de273ac02a59fb9b74487803cce2
Christopher Chang <christopherchang9@gmail.com>
2018-05-15 20:10:57 -0400

    AccidentalMap complete

--- src/karaoke/songs/AccidentalMap.java
+++ src/karaoke/songs/AccidentalMap.java
@@ -10,19 +10,40 @@ public class AccidentalMap {
     private final Map<String, Integer> keySignature;
     private final Map<Pitch, Integer> accidentals = new HashMap<>();
     
+    /**
+     * Create a new AccidentalMap for the given key signature.
+     * @param keySignature key signature, which contains the sharps and flats of a key
+     */
     public AccidentalMap(Map<String, Integer> keySignature) {
         this.keySignature = new HashMap<>(keySignature);
     }
     
+    /**
+     * Update this accidental map with a new accidental.
+     * @param pitch note on which an accidental was found, must be a white note
+     * @param accidental accidental on note in signed half steps
+     */
     public void setAccidental(Pitch pitch, int accidental) {
         accidentals.put(pitch, accidental);
     }
     
+    /**
+     * @param pitch a white note
+     * @return accidental on given note according to this accidental map
+     */
     public int getAccidental(Pitch pitch) {
         if (accidentals.containsKey(pitch))
             return accidentals.get(pitch);
         else
             return keySignature.getOrDefault(pitch.note(), 0);
+    }
+    
+    /**
+     * Removes all accidentals encountered during a measure, leaving only the underlying key signature.
+     * Should be called every measure.
+     */
+    public void refresh() {
+        accidentals.clear();
     }
     
 }

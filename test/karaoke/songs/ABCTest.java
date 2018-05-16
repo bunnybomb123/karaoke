4932289da3e3ed3cbf45a50d5ac3d16c743320db
Your Full Name <ericaw@mit.edu>
2018-05-15 23:38:40 -0700

    ABCParserTest

--- test/karaoke/songs/ABCTest.java
+++ test/karaoke/songs/ABCTest.java
@@ -1,6 +1,14 @@
 package karaoke.songs;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
+
+import karaoke.music.Music;
+import karaoke.music.Rest;
 
 public class ABCTest {
 
@@ -22,6 +30,37 @@ public class ABCTest {
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
+    }
+    
+    /* helper */
+    private void helper(Map<Character, Object> expected, ABC actual) {
+    	assertEquals(expected.get('T'), actual.getTitle());
+    	assertEquals(expected.get('Q'), actual.getTempo());
+    	assertEquals(expected.get('K'), actual.getKeySignature());
+    	assertEquals(expected.get('M'), actual.getMeter());
+    	assertEquals(expected.get('V'), actual.getVoices());
+    	assertEquals(expected.get('C'), actual.getComposer());
+    	assertEquals(expected.get('X'), actual.getComposer());
+    }
+    
+    // M, L, and Q are omitted
+    @Test
+    public void testABC() {
+    	final Map<String, Music> parts = new HashMap<>();
+    	Music music = new Rest(0);
+        parts.put("", music);
+        
+        final Map<Character, Object> expected = new HashMap<>();
+        expected.put('T', "hi");
+        expected.put('K', Key.valueOf("C"));
+        expected.put('X', 1);
+        
+//        HashMap<Character, Object> expected = new HashMap<>(fields);
+        expected.put('M', new Meter(4, 4));
+        expected.put('Q', 100);
+
+        ABC actual = new ABC(parts, expected);
+        helper(expected, actual);
     }
     
     
